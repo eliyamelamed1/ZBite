9f68906c3d851470616f7bdc484a92bdac2f587d
moneyman01 <tchrocr@gmail.com>
2021-06-08 15:40:28 +0300

    g

--- front-end/src/components/recipes/DisplayRecipes.js
+++ front-end/src/components/recipes/DisplayRecipes.js
@@ -1,40 +1,42 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import RecipeCard from './RecipeCard';
-
+import PropTypes from "prop-types";
+import React from "react";
+import RecipeCard from "./RecipeCard";
+//שינוי לגיט
 const displayRecipes = ({ recipes }) => {
-    const getRecipes = () => {
-        const recipesOnPage = [];
-        const result = [];
+  const getRecipes = () => {
+    const recipesOnPage = [];
+    const result = [];
 
-        recipes.map((recipe) =>
-            recipesOnPage.push(
-                <RecipeCard
-                    title={recipe.title}
-                    flavor_type={recipe.flavor_type}
-                    photo_main={recipe.photo_main}
-                    id={recipe.id}
-                    author={recipe.author}
-                />
-            )
-        );
+    recipes.map((recipe) =>
+      recipesOnPage.push(
+        <RecipeCard
+          title={recipe.title}
+          flavor_type={recipe.flavor_type}
+          photo_main={recipe.photo_main}
+          id={recipe.id}
+          author={recipe.author}
+        />
+      )
+    );
 
-        // 3 is for 3 recipes on page
-        for (let i = 0; i < recipes.length; i += 1) {
-            result.push(
-                <div key={i}>
-                    <div>{recipesOnPage[i] ? recipesOnPage[i] : null}</div>
-                </div>
-            );
-        }
+    // 3 is for 3 recipes on page
+    for (let i = 0; i < recipes.length; i += 1) {
+      result.push(
+        <div key={i}>
+          <div>{recipesOnPage[i] ? recipesOnPage[i] : null}</div>
+        </div>
+      );
+    }
 
-        return result;
-    };
-    return <div data-testid='displayRecipes'>{recipes ? getRecipes() : null}</div>;
+    return result;
+  };
+  return (
+    <div data-testid="displayRecipes">{recipes ? getRecipes() : null}</div>
+  );
 };
 
 displayRecipes.propTypes = {
-    recipes: PropTypes.array.isRequired,
+  recipes: PropTypes.array.isRequired,
 };
 
 export default displayRecipes;
