d6c8cb78bff69aad3706cf7883c5fda23df1132c
eliyamelamed1 <70061884+eliyamelamed1@users.noreply.github.com>
2021-06-24 06:14:41 +0300

    Update UserDelete.js

--- front-end/src/components/users/UserDelete.js
+++ front-end/src/components/users/UserDelete.js
@@ -1,5 +1,6 @@
-// TODO - change PropTypes id to .isRequired
-// TODO - check the redirect
+/* - test action have dispatched
+ test the redirect
+ */
 
 import { connect, useDispatch } from 'react-redux';
 
