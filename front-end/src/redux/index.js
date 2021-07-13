c3d6f83d7107f483ad506fcb0dc49f49b0bb2f3a
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-07-14 00:02:15 +0300

    removed sockets+chat front-end code

--- front-end/src/redux/index.js
+++ front-end/src/redux/index.js
@@ -1,12 +1,8 @@
 import authReducer from './reducers/auth';
-import chatReducer from './reducers/chat';
 import { combineReducers } from 'redux';
 import recipeReducer from './reducers/recipe';
-import socketReducer from './reducers/socket';
 
 export default combineReducers({
     authReducer,
     recipeReducer,
-    socketReducer,
-    chatReducer,
 });
