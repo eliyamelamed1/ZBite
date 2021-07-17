63f05f3a908f52100f9b03356f3f6cfe2cd25776
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-07-18 00:10:10 +0300

    fixed withRedux err

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
-import { wrapper } from '../redux/store';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Provider>
     );
 }
-export default wrapper.withRedux(MyApp);
+export default MyApp;
