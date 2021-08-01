dc387078f209990c8e46fa5276f78f8cacf3dee6
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-08-02 00:24:40 +0300

    note on how to improve followUnFollowAction

--- frontend/redux/actions/follower.tsx
+++ frontend/redux/actions/follower.tsx
@@ -16,6 +16,10 @@ export const followUnFollowAction =
             };
             const body = JSON.stringify({ user_to_follow });
             const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/followers/follow/`, body, config);
+            // if (res.data === 'followed')
+            // dispatch type FOLLOW_USER_SUCCESS => (add user to loggedUserData.following)
+            // else
+            // dispatch type UNFOLLOW_USER_SUCCESS => (remove user from loggedUserData.following)
             dispatch({ type: FOLLOW_UNFOLLOW_USER_SUCCESS, payload: res.data });
             dispatch(loadUserDetailsAction({ id: user_to_follow }));
             dispatch(loadloggedUserDataAction());
