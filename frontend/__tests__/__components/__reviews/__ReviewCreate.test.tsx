2afce64918397ea01fcb40976e9993c315282f31
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-08-31 00:13:18 +0300

    CHECKPOINT

--- frontend/__tests__/__components/__reviews/__ReviewCreate.test.tsx
+++ frontend/__tests__/__components/__reviews/__ReviewCreate.test.tsx
@@ -2,10 +2,10 @@ import '@testing-library/jest-dom/extend-expect';
 
 describe('ReviewCreate', () => {
     describe('authenticated Users', () => {
-        test('should render without crashing', () => {});
+        test.todo('should render without crashing');
         describe('test toggling between creating/editing a review ', () => {
-            test('should display "Create a review" button if user doesnt have a review', () => {});
-            test('should display "Edit a review" button user have a review', () => {});
+            test.todo('should display "Create a review" button if user doesnt have a review');
+            test.todo('should display "Edit a review" button user have a review');
         });
 
         describe('recipe input', () => {});
@@ -14,14 +14,14 @@ describe('ReviewCreate', () => {
         describe('image input', () => {});
 
         describe('submit button', () => {
-            test('should render submit button  ', () => {});
-            test('success in create/editing a review should dispatch reviewCreateAction  ', () => {});
-            test('failure in create/editing a review should not dispatch reviewCreateAction  ', () => {});
+            test.todo('should render submit button');
+            test.todo('success in create/editing a review should dispatch reviewCreateAction');
+            test.todo('failure in create/editing a review should not dispatch reviewCreateAction');
         });
     });
 
     describe('guest Users', () => {
-        test('should render without crashing', () => {});
-        test('should redirect to login page', () => {});
+        test.todo('should render without crashing');
+        test.todo('should redirect to login page');
     });
 });
