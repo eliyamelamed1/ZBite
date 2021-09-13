8b0fc4a6cd3cace0df87a26d1eb6e5e7ee5d9fe7
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-09-13 12:48:41 +0300

    finished testing reducers

--- frontend/__tests__/__redux/__reducers/__recipeReducer.test.tsx
+++ frontend/__tests__/__redux/__reducers/__recipeReducer.test.tsx
@@ -6,6 +6,13 @@ import '@testing-library/jest-dom/extend-expect';
 import { cleanup } from '@testing-library/react';
 import store from '../../../redux/store';
 
+const updatedState = {
+    listOfRecipes: 'updatedState',
+    listOfSearchedRecipes: 'updatedState',
+    requestedRecipeData: 'updatedState',
+    listOfFilteredReviews: 'updatedState',
+};
+
 describe('recipeReducers - cases that modify the state', () => {
     let initialState;
     beforeEach(() => {
@@ -22,70 +29,53 @@ describe('recipeReducers - cases that modify the state', () => {
     });
 
     test('case SEARCH_RECIPE_SUCCESS', () => {
-        initialState['listOfSearchedRecipes'] = {
-            firstRecipe: { title: 'testTitle' },
-            secondRecipe: { title: 'testTitle2' },
-        };
-        store.dispatch({ type: 'SEARCH_RECIPE_SUCCESS', payload: initialState.listOfSearchedRecipes });
+        store.dispatch({ type: 'SEARCH_RECIPE_SUCCESS', payload: updatedState.listOfSearchedRecipes });
         const storeState = store.getState();
 
         expect(storeState.recipeReducer.listOfRecipes).toBeNull();
-        expect(storeState.recipeReducer.listOfSearchedRecipes).toStrictEqual(initialState.listOfSearchedRecipes);
+        expect(storeState.recipeReducer.listOfSearchedRecipes).toStrictEqual(updatedState.listOfSearchedRecipes);
         expect(storeState.recipeReducer.requestedRecipeData).toBeNull();
         expect(storeState.recipeReducer.listOfFilteredReviews).toBeNull();
     });
     test('case GET_RECIPE_DETAILS_SUCCESS', () => {
-        initialState['requestedRecipeData'] = {
-            firstRecipe: { title: 'testTitle' },
-            secondRecipe: { title: 'testTitle2' },
-        };
-        store.dispatch({ type: 'GET_RECIPE_DETAILS_SUCCESS', payload: initialState.requestedRecipeData });
+        store.dispatch({ type: 'GET_RECIPE_DETAILS_SUCCESS', payload: updatedState.requestedRecipeData });
         const storeState = store.getState();
 
         expect(storeState.recipeReducer.listOfRecipes).toBeNull();
-        expect(storeState.recipeReducer.requestedRecipeData).toStrictEqual(initialState.requestedRecipeData);
+        expect(storeState.recipeReducer.requestedRecipeData).toStrictEqual(updatedState.requestedRecipeData);
         expect(storeState.recipeReducer.listOfSearchedRecipes).toBeNull();
         expect(storeState.recipeReducer.listOfFilteredReviews).toBeNull();
     });
     test('case UPDATE_RECIPE_SUCCESS', () => {
-        initialState['requestedRecipeData'] = {
-            firstRecipe: { title: 'testTitle' },
-            secondRecipe: { title: 'testTitle2' },
-        };
-        store.dispatch({ type: 'UPDATE_RECIPE_SUCCESS', payload: initialState.requestedRecipeData });
+        store.dispatch({ type: 'UPDATE_RECIPE_SUCCESS', payload: updatedState.requestedRecipeData });
         const storeState = store.getState();
 
         expect(storeState.recipeReducer.listOfRecipes).toBeNull();
         expect(storeState.recipeReducer.listOfSearchedRecipes).toBeNull();
-        expect(storeState.recipeReducer.requestedRecipeData).toStrictEqual(initialState.requestedRecipeData);
+        expect(storeState.recipeReducer.requestedRecipeData).toStrictEqual(updatedState.requestedRecipeData);
         expect(storeState.recipeReducer.listOfFilteredReviews).toBeNull();
     });
     test('case REVIEWS_IN_RECIPE_SUCCESS', () => {
-        initialState['listOfFilteredReviews'] = [{ id: 'firstReviewId' }, { id: 'secondReviewId' }];
-        store.dispatch({ type: 'REVIEWS_IN_RECIPE_SUCCESS', payload: initialState.listOfFilteredReviews });
+        store.dispatch({ type: 'REVIEWS_IN_RECIPE_SUCCESS', payload: updatedState.listOfFilteredReviews });
         const storeState = store.getState();
 
         expect(storeState.recipeReducer.listOfRecipes).toBeNull();
         expect(storeState.recipeReducer.listOfSearchedRecipes).toBeNull();
         expect(storeState.recipeReducer.requestedRecipeData).toBeNull();
-        expect(storeState.recipeReducer.listOfFilteredReviews).toStrictEqual(initialState.listOfFilteredReviews);
+        expect(storeState.recipeReducer.listOfFilteredReviews).toStrictEqual(updatedState.listOfFilteredReviews);
     });
     test('case GET_RECIPE_LIST_SUCCESS', () => {
-        initialState['listOfRecipes'] = {
-            firstRecipe: { title: 'testTitle' },
-            secondRecipe: { title: 'testTitle2' },
-        };
-        store.dispatch({ type: 'GET_RECIPE_LIST_SUCCESS', payload: initialState.listOfRecipes });
+        store.dispatch({ type: 'GET_RECIPE_LIST_SUCCESS', payload: updatedState.listOfRecipes });
         const storeState = store.getState();
 
-        expect(storeState.recipeReducer.listOfRecipes).toStrictEqual(initialState.listOfRecipes);
+        expect(storeState.recipeReducer.listOfRecipes).toStrictEqual(updatedState.listOfRecipes);
         expect(storeState.recipeReducer.listOfSearchedRecipes).toBeNull();
         expect(storeState.recipeReducer.requestedRecipeData).toBeNull();
         expect(storeState.recipeReducer.listOfFilteredReviews).toBeNull();
     });
 });
 
-describe('recipeReducers - : cases that return ...state', () => {
+describe('recipeReducers - cases that return ...state => state should not be modified', () => {
     let initialState;
     beforeEach(() => {
         cleanup();
@@ -104,46 +94,46 @@ describe('recipeReducers - : cases that return ...state', () => {
         expect(storeState.recipeReducer).toEqual(initialState);
     });
     test('case DELETE_RECIPE_SUCCESS ', () => {
-        store.dispatch({ type: 'DELETE_RECIPE_SUCCESS', payload: initialState });
+        store.dispatch({ type: 'DELETE_RECIPE_SUCCESS', payload: updatedState });
     });
 
     test('case DELETE_RECIPE_FAIL ', () => {
-        store.dispatch({ type: 'DELETE_RECIPE_FAIL', payload: initialState });
+        store.dispatch({ type: 'DELETE_RECIPE_FAIL', payload: updatedState });
     });
     test('case CREATE_RECIPE_SUCCESS ', () => {
-        store.dispatch({ type: 'CREATE_RECIPE_SUCCESS', payload: initialState });
+        store.dispatch({ type: 'CREATE_RECIPE_SUCCESS', payload: updatedState });
     });
     test('case CREATE_RECIPE_FAIL ', () => {
-        store.dispatch({ type: 'CREATE_RECIPE_FAIL', payload: initialState });
+        store.dispatch({ type: 'CREATE_RECIPE_FAIL', payload: updatedState });
     });
     test('case UPDATE_RECIPE_FAIL ', () => {
-        store.dispatch({ type: 'UPDATE_RECIPE_FAIL', payload: initialState });
+        store.dispatch({ type: 'UPDATE_RECIPE_FAIL', payload: updatedState });
     });
     test('case GET_RECIPE_DETAILS_FAIL ', () => {
-        store.dispatch({ type: 'GET_RECIPE_DETAILS_FAIL', payload: initialState });
+        store.dispatch({ type: 'GET_RECIPE_DETAILS_FAIL', payload: updatedState });
     });
     test('case GET_RECIPE_LIST_FAIL ', () => {
-        store.dispatch({ type: 'GET_RECIPE_LIST_FAIL', payload: initialState });
+        store.dispatch({ type: 'GET_RECIPE_LIST_FAIL', payload: updatedState });
     });
     test('case SEARCH_RECIPE_FAIL ', () => {
-        store.dispatch({ type: 'SEARCH_RECIPE_FAIL', payload: initialState });
+        store.dispatch({ type: 'SEARCH_RECIPE_FAIL', payload: updatedState });
     });
     test('case REVIEW_CREATE_SUCCESS ', () => {
-        store.dispatch({ type: 'REVIEW_CREATE_SUCCESS', payload: initialState });
+        store.dispatch({ type: 'REVIEW_CREATE_SUCCESS', payload: updatedState });
     });
     test('case REVIEW_CREATE_FAIL ', () => {
-        store.dispatch({ type: 'REVIEW_CREATE_FAIL', payload: initialState });
+        store.dispatch({ type: 'REVIEW_CREATE_FAIL', payload: updatedState });
     });
     test('case REVIEW_DELETE_SUCCESS ', () => {
-        store.dispatch({ type: 'REVIEW_DELETE_SUCCESS', payload: initialState });
+        store.dispatch({ type: 'REVIEW_DELETE_SUCCESS', payload: updatedState });
     });
     test('case REVIEW_DELETE_FAIL ', () => {
-        store.dispatch({ type: 'REVIEW_DELETE_FAIL', payload: initialState });
+        store.dispatch({ type: 'REVIEW_DELETE_FAIL', payload: updatedState });
     });
     test('case REVIEWS_IN_RECIPE_FAIL ', () => {
-        store.dispatch({ type: 'REVIEWS_IN_RECIPE_FAIL', payload: initialState });
+        store.dispatch({ type: 'REVIEWS_IN_RECIPE_FAIL', payload: updatedState });
     });
     test('case - default', () => {
-        store.dispatch({ type: 'default', payload: initialState });
+        store.dispatch({ type: 'default', payload: updatedState });
     });
 });
