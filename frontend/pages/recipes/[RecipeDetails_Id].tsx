a69a6a3b06e3d6d056bd6e3352301a7aeeb15a8e
eliyamelamed1 <eliyamelamed1@gmail.com>
2021-09-26 20:09:48 +0300

    added saves count to RecipeDetails

--- frontend/pages/recipes/[RecipeDetails_Id].tsx
+++ frontend/pages/recipes/[RecipeDetails_Id].tsx
@@ -77,6 +77,7 @@ const RecipeDetails = (props) => {
                                     recipe Author: <p>{recipeData.author}</p>
                                 </div>
                             </Link>
+                            <p>saves: {recipeData.saves.length}</p>
                             <div>
                                 recipe title: <h1>{recipeData.title}</h1>
                             </div>
