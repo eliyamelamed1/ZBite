1eb8efdb410f5f67fdf06c57ddc5d7dfe2ff63b6
Eliya Melamed <eliyamelamed1@gmail.com>
2021-10-24 13:46:48 +0300

    refractored _navbar.scss to _navbar.module.scss

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,7 +11,7 @@ import PlusIcon from '../styles/icons/plus.svg';
 import ProfileIcon from '../styles/icons/profile.svg';
 import SavedIcon from '../styles/icons/heart.svg';
 import SearchIcon from '../styles/icons/search.svg';
-import SideNav from './ui/SideNav';
+import classes from '../styles/layout/_navbar.module.scss';
 import { logoutAction } from '../redux/actions/userActions';
 import { pageRoute } from '../globals';
 
@@ -39,56 +39,56 @@ const Navbar = () => {
     };
 
     const NavbarLinks = (
-        <nav data-testid='NavbarLinks'>
-            <ul className='nav__list'>
+        <nav data-testid='NavbarLinks' className={classes.nav}>
+            <ul className={classes.nav__list}>
                 <li>
                     <Link href={`${pageRoute().home}`}>
-                        <a className='nav__link'>
-                            <i className='nav__link-icon'>
+                        <a className={classes.nav__link}>
+                            <i className={classes.nav__link__icon}>
                                 {HomeIcon.src && <Image src={HomeIcon} alt='as' height={50} width={60} />}
                             </i>
-                            <p className='nav__link-text'>Home</p>
+                            <p className={classes.nav__link__text}>Home</p>
                         </a>
                     </Link>
                 </li>
                 <li>
                     <Link href='/'>
-                        <a className='nav__link'>
-                            <i className='nav__link-icon'>
+                        <a className={classes.nav__link}>
+                            <i className={classes.nav__link__icon}>
                                 {SavedIcon.src && <Image src={SavedIcon} alt='as' height={50} width={60} />}
                             </i>
-                            <p className='nav__link-text'>Saved</p>
+                            <p className={classes.nav__link__text}>Saved</p>
                         </a>
                     </Link>
                 </li>
-                <li className='nav__item create-recipe'>
+                <li className={`${classes.nav__item} ${classes.create__recipe}`}>
                     <Link href='/'>
-                        <a className='nav__link create-recipe'>
-                            <i className='nav__link-icon create-recipe'>
+                        <a className={`${classes.nav__link} ${classes.create__recipe}`}>
+                            <i className={`${classes.nav__link__icon} ${classes.create__recipe}`}>
                                 {PlusIcon.src && <Image src={PlusIcon} alt='as' height={50} width={60} />}
                             </i>
-                            <p className='nav__link-text create-recipe'>Create Recipe</p>
+                            <p className={`${classes.nav__link__text} ${classes.create__recipe}`}>Create Recipe</p>
                         </a>
                     </Link>
                 </li>
                 <li>
                     <Link href='/'>
                         <a>
-                            <i className='nav__link-icon'>
+                            <i className={classes.nav__link__icon}>
                                 {LeaderboardIcon.src && <Image src={LeaderboardIcon} alt='as' height={50} width={60} />}
                             </i>
 
-                            <p className='nav__link-text'>Leaderboard</p>
+                            <p className={classes.nav__link__text}>Leaderboard</p>
                         </a>
                     </Link>
                 </li>
                 <li>
                     <Link href={profileUrl}>
                         <a>
-                            <i className='nav__link-icon'>
+                            <i className={classes.nav__link__icon}>
                                 {ProfileIcon.src && <Image src={ProfileIcon} alt='as' height={50} width={60} />}
                             </i>
-                            <p className='nav__link-text'>Profile</p>
+                            <p className={classes.nav__link__text}>Profile</p>
                         </a>
                     </Link>
                 </li>
@@ -96,20 +96,18 @@ const Navbar = () => {
         </nav>
     );
     const authenitcationLinks = (
-        <section className='auth__links'>
+        <section className={classes.auth__links}>
             {isUserAuthenticated ? (
-                <ul className='auth__links-list'>
-                    <button onClick={logoutHandler} className='logout-btn'>
-                        Logout
-                    </button>
+                <ul className={classes.auth__links__list}>
+                    <button onClick={logoutHandler}>Logout</button>
                 </ul>
             ) : (
-                <ul className='auth__links-list'>
-                    <li className='login-btn'>
+                <ul className={classes.auth__links__list}>
+                    <li>
                         <Link href='/users/UserLogin'>Login</Link>
                     </li>
                     <br />
-                    <li className='signup-btn'>
+                    <li>
                         <Link href='/users/UserSignup'>Sign Up</Link>
                     </li>
                 </ul>
@@ -118,14 +116,14 @@ const Navbar = () => {
     );
 
     return (
-        <div data-testid='navbar' className='container'>
-            <header>
-                <i className='logo-icon'>
+        <div data-testid='navbar' className={classes.container}>
+            <header className={classes.header}>
+                <i className={classes.logo__icon}>
                     {LogoIcon.src && <Image src={LogoIcon} alt='logo icon' height={100} width={100} />}
                 </i>
-                <div className='search-box'>
-                    <input type='text' className='search-txt' placeholder='Search' />
-                    <button className='search-btn'>
+                <div className={classes.search__box}>
+                    <input type='text' className={classes.search__txt} placeholder='Search' />
+                    <button className={classes.search__btn}>
                         <i>{SearchIcon.src && <Image src={SearchIcon} alt='search icon' height={100} width={100} />}</i>
                     </button>
                 </div>
