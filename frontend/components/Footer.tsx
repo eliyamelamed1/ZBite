5203e1635fc58e12c6020399aacd0603b58e4e1d
Eliya Melamed <eliyamelamed1@gmail.com>
2021-10-24 14:20:49 +0300

    refractprored footer to footer.module

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import LogoIcon from '../styles/icons/logo.svg';
+import styles from '../styles/layout/_footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <ul>
                 <li>
                     <i>{LogoIcon.src && <Image src={LogoIcon} alt='logo icon' height={100} width={100} />}</i>
