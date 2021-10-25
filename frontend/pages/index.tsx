994cbe8f09fd09e9aaa7011253290d9d8d7d6a3a
Eliya Melamed <eliyamelamed1@gmail.com>
2021-10-25 14:42:40 +0300

    designed home page recipes layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react';
 
+import DisplayRecipes from '../components/recipes/DisplayRecipes';
 import classes from '../styles/pages/home.module.scss';
+import { loadRecipeListAction } from '../redux/actions/recipeActions';
+import store from '../redux/store';
 
-const HomePage = () => {
+const HomePage = (props) => {
     const buttonsRef = useRef() as any;
     const onClick = (e) => {
         if (e.target.name === 'subscribed') {
@@ -15,6 +18,7 @@ const HomePage = () => {
         }
     };
 
+    const listOfRecipes = props.listOfRecipes;
     return (
         <div className={classes.container}>
             <ul className={classes.recipe_filter} ref={buttonsRef} onClick={onClick}>
@@ -23,8 +27,37 @@ const HomePage = () => {
                 </button>
                 <button name='for you'>For You</button>
             </ul>
+            <ul className={classes.recipes_container}>
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+                {listOfRecipes && <DisplayRecipes recipesToDisplay={listOfRecipes} />}
+            </ul>
         </div>
     );
 };
+
+export async function getStaticProps() {
+    await store.dispatch(loadRecipeListAction());
+    const { listOfRecipes } = store.getState().recipeReducer;
+
+    return { props: { listOfRecipes }, revalidate: 10 };
+}
 
 export default HomePage;
