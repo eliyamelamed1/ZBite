416c520762687d32cc99c71f99321933abb6c59f
Eliya Melamed <eliyamelamed1@gmail.com>
2021-10-25 13:44:58 +0300

    added local css to git ignore: (css that generated by live sass compiler extention

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,16 @@
-// import '../../dist/css/main.css';
-import '../styles/main.scss';
+import '../../dist/css/main.css';
 
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+
+// import '../styles/main.scss';
+
+
+
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
