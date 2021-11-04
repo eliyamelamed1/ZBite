093dc25af12c4f443ac55aaa582eb693f455370d
Eliya Melamed <eliyamelamed1@gmail.com>
2021-11-04 14:26:53 +0200

    .

---
+++ frontend/pages/recipes/SavedRecipes.tsx
@@ -0,0 +1,9 @@
+import store from '../../redux/store';
+
+const SavedRecipes = () => {};
+
+export async function getServerSideProps() {
+    // await store.dispatch()
+}
+
+export default SavedRecipes;
