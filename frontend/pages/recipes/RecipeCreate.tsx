faede401a480614c9ddba24c3adce691ef6d80b7
Eliya Melamed <eliyamelamed1@gmail.com>
2021-11-17 14:50:26 +0200

    Instructions section functionality all set up (need refractoring)

--- frontend/pages/recipes/RecipeCreate.tsx
+++ frontend/pages/recipes/RecipeCreate.tsx
@@ -4,8 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import Image from 'next/image';
 import Router from 'next/router';
 import deleteIcon from '../../styles/icons/delete-input-icon.svg';
+import editInput from '../../styles/icons/edit_input.svg';
 import { pageRoute } from '../../globals';
 import { recipeCreateAction } from '../../redux/actions/recipeActions';
+import saveInput from '../../styles/icons/save_changes.svg';
 import styles from '../../styles/pages/recipeCreate.module.scss';
 import uploadImageIcon from '../../styles/icons/upload_image.png';
 
@@ -26,7 +28,7 @@ const RecipeCreate = () => {
     const { isUserAuthenticated } = useSelector((state) => state.userReducer);
     isUserAuthenticated === false ? Router.push(pageRoute().home) : null;
 
-    const onChangeText = (e) => setData((prevState) => ({ ...data, [e.target.name]: e.target.value }));
+    const onChangeText = (e) => setData((data) => ({ ...data, [e.target.name]: e.target.value }));
     const onChangeImage = (e) => {
         try {
             const imageSrc = URL.createObjectURL(e.target.files[0]);
@@ -42,6 +44,7 @@ const RecipeCreate = () => {
             // console.log(err);
         }
     };
+
     const generalSection = () => (
         <section className={styles.general_section}>
             <input
@@ -107,66 +110,67 @@ const RecipeCreate = () => {
             </div>
         </section>
     );
+
+    // -------new code
+    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState('');
+    const [todoEditing, setTodoEditing] = React.useState(null);
+    const [editingText, setEditingText] = React.useState('');
+
     const instructionSection = () => (
-        <section className={styles.instructions_section}>
+        <section id='todo-list' className={styles.instructions_section}>
             <h1 className={styles.instructions_title}>Instructions</h1>
-            <li className={styles.instructions_input_container}>
-                <input
-                    type='text'
-                    placeholder='Cut the meat'
-                    name='instructions'
-                    // value={instructions}
-                    onChange={onChangeText}
-                    className={styles.instruction_input}
-                    required
-                />
-                <i className={styles.delete_button}>
-                    {deleteIcon.src && <Image src={deleteIcon.src} alt='delete icon' width={50} height={60} />}
-                </i>
-            </li>
-            <li>
-                <input
-                    id='instruction_image'
-                    type='file'
-                    placeholder='image'
-                    name='image'
-                    onChange={onChangeText}
-                    className={styles.image_input}
-                />
-                <label htmlFor='instruction_image' className={styles.image_label}>
-                    <Image src={uploadImageIcon} width={100} height={100} alt='recipe photo' />
-                    <span className={styles.image_text}>Add image</span>
-                </label>
-            </li>
-            <button className={styles.add_instruction} type='button'>
-                + instruction
+            <input
+                type='text'
+                onChange={(e) => setTodo(e.target.value)}
+                value={todo}
+                className={styles.instruction_input}
+            />
+            <button onClick={handleSubmit} type='button' className={styles.add_instruction}>
+                + Instruction
             </button>
-        </section>
-    );
-    const ingredientSection = () => (
-        <section className={styles.ingredients_section}>
-            <h1 className={styles.ingredient_title}>Ingredients</h1>
-            <li className={styles.ingredients_input_container}>
-                <input
-                    type='text'
-                    placeholder='250g flour'
-                    name='ingredients'
-                    value={ingredients}
-                    onChange={onChangeText}
-                    className={styles.ingredient_input}
-                    required
-                />
-                <button className={styles.delete_button}>
-                    {deleteIcon.src && <Image src={deleteIcon.src} alt='delete icon' width={50} height={60} />}
-                </button>
-            </li>
-            <button className={styles.add_ingredient}>+ Ingredient</button>
+            {todos.map((todo) => (
+                <section key={todo.id} className={styles.new_instruction}>
+                    <div className={styles.new_instruction}>
+                        {todo.id === todoEditing ? (
+                            <input
+                                type='text'
+                                onChange={(e) => setEditingText(e.target.value)}
+                                className={styles.instruction_input}
+                            />
+                        ) : (
+                            <div className={styles.instruction_input}>{todo.text}</div>
+                        )}
+                    </div>
+                    <div className={styles.instructions_actions}>
+                        {todo.id === todoEditing ? (
+                            <button onClick={() => submitEdits(todo.id)} type='button' className={styles.save_button}>
+                                {saveInput.src && (
+                                    <Image src={saveInput.src} alt='delete icon' width={50} height={60} />
+                                )}
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => setTodoEditing(todo.id)}
+                                className={styles.edit_button}
+                                type='button'
+                            >
+                                {editInput.src && (
+                                    <Image src={editInput.src} alt='delete icon' width={50} height={60} />
+                                )}
+                            </button>
+                        )}
+                        <button onClick={() => deleteTodo(todo.id)} className={styles.delete_button} type='button'>
+                            {deleteIcon.src && <Image src={deleteIcon.src} alt='delete icon' width={50} height={60} />}
+                        </button>
+                    </div>
+                </section>
+            ))}
         </section>
     );
 
-    // new code
     function handleSubmit(e) {
-        e.preventDefault();
+        if (todo == '') return;
 
         const newTodo = {
             id: new Date().getTime(),
@@ -183,50 +187,18 @@ const RecipeCreate = () => {
     }
 
     function submitEdits(id) {
+        if (editingText === '') return;
+
         const updatedTodos = [...todos].map((todo) => {
             if (todo.id === id) {
                 todo.text = editingText;
             }
+
             return todo;
         });
         setTodos(updatedTodos);
         setTodoEditing(null);
     }
-
-    const [todos, setTodos] = React.useState([]);
-    const [todo, setTodo] = React.useState('');
-    const [todoEditing, setTodoEditing] = React.useState(null);
-    const [editingText, setEditingText] = React.useState('');
-
-    const newCodeSection = () => (
-        <div id='todo-list'>
-            <h1>Todo List</h1>
-            <form onSubmit={handleSubmit}>
-                <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo} />
-                <button type='submit'>Add Todo</button>
-            </form>
-            {todos.map((todo) => (
-                <div key={todo.id} className='todo'>
-                    <div className='todo-text'>
-                        {todo.id === todoEditing ? (
-                            <input type='text' onChange={(e) => setEditingText(e.target.value)} />
-                        ) : (
-                            <div>{todo.text}</div>
-                        )}
-                    </div>
-                    <div className='todo-actions'>
-                        {todo.id === todoEditing ? (
-                            <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
-                        ) : (
-                            <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
-                        )}
-
-                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
 
     return (
         <div data-testid='recipeCreate' className={styles.container}>
@@ -235,13 +207,10 @@ const RecipeCreate = () => {
                 <hr className={styles.section_separator} />
                 {instructionSection()}
                 <hr className={styles.section_separator} />
-                {ingredientSection()}
-                <hr className={styles.section_separator} />
                 <button type='submit' className={styles.create_button}>
                     Create Recipe
                 </button>
             </form>
-            {newCodeSection()}
         </div>
     );
 };
