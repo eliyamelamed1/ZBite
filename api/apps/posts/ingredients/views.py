7433e20624c98464af67883807ae322aa19276ee
eliyamelamed1 <eliyamelamed1@email.com>
2021-12-02 00:06:47 +0200

    checkpoint

--- api/apps/posts/ingredients/views.py
+++ api/apps/posts/ingredients/views.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,6 +13,5 @@ class IngredientCreate(CreateAPIView):
     queryset = Ingredient.objects.all()
 
     def perform_create(self, serializer):
-        '''save the the current logged in user as the author of the recipe'''
+        '''save the the current logged in user as the author'''
         serializer.save(author=self.request.user)
-
