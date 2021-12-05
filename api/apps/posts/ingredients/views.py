1e27611a9513afe965c507ff35d508e1227b8640
eliyamelamed1 <eliyamelamed1@email.com>
2021-12-05 16:38:01 +0200

    simplified Instruction and Ingredient views

--- api/apps/posts/ingredients/views.py
+++ api/apps/posts/ingredients/views.py
@@ -15,29 +15,17 @@ class IngredientCreate(CreateAPIView):
     serializer_class = IngredientCreateSerializer
     queryset = Ingredient.objects.all()
 
-    def create(self, request, format=None):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        input_recipe_id = self.request.data['recipe']
-
-        recipe = Recipe.objects.get(id__exact=input_recipe_id)
-        user = self.request.user
-
-        is_recipe_author = recipe.author == user
-        if not is_recipe_author: raise PermissionDenied(detail='You are no the author')
-        
-        self.perform_create(serializer)
-
-        return HttpResponse(status=201)
-
     def perform_create(self, serializer):
         '''save the the current logged in user as the author'''
         obj = serializer.save(author=self.request.user)
 
-        input_recipe_id = self.request.data['recipe']
-        recipe = Recipe.objects.get(id__exact=input_recipe_id)
-        recipe.ingredients = obj
-        recipe.save()
+        is_recipe_author = obj.recipe.author == self.request.user
+        if not is_recipe_author: 
+            obj.delete()
+            raise PermissionDenied(detail='You are not the recipe author')
+
+        obj.recipe.ingredients = obj
+        obj.recipe.save()
 
 class IngredientDetails(RetrieveUpdateAPIView):
     permission_classes = (IsRecipeAuthorOrIngredientModifyDenied,)
@@ -47,7 +35,5 @@ class IngredientDetails(RetrieveUpdateAPIView):
     def perform_update(self, serializer):
         obj = serializer.save()
 
-        input_recipe_id = self.request.data['recipe']
-        recipe = Recipe.objects.get(id__exact=input_recipe_id)
-        recipe.ingredients = obj
-        recipe.save()
+        obj.recipe.ingredients = obj
+        obj.recipe.save()
--- api/apps/posts/instructions/views.py
+++ api/apps/posts/instructions/views.py
@@ -6,7 +6,6 @@ from rest_framework.generics import (CreateAPIView, RetrieveUpdateAPIView,)
 from apps.posts.instructions.models import Instruction
 from apps.posts.instructions.serializers import (InstructionCreateSerializer,
                                                  InstructionUpdateSerializer)
-from apps.posts.recipes.models import Recipe
 from permissions import IsRecipeAuthorOrIngredientModifyDenied
 
 
@@ -14,31 +13,18 @@ class InstructionCreate(CreateAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = InstructionCreateSerializer
     queryset = Instruction.objects.all()
-
-    def create(self, request, format=None):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        recipes_queryset = Recipe.objects.all()
-        input_recipe_id = self.request.data['recipe']
-
-        recipe = recipes_queryset.get(id__exact=input_recipe_id)
-        user = self.request.user
-
-        is_recipe_author = recipe.author == user
-        if not is_recipe_author: raise PermissionDenied(detail='You are no the author')
-        
-        self.perform_create(serializer)
-
-        return HttpResponse(status=201)
     
     def perform_create(self, serializer):
         '''save the the current logged in user as the author'''
         obj = serializer.save(author=self.request.user)
 
-        input_recipe_id = self.request.data['recipe']
-        recipe = Recipe.objects.get(id__exact=input_recipe_id)
-        recipe.instructions = obj
-        recipe.save()
+        is_recipe_author = obj.recipe.author == self.request.user
+        if not is_recipe_author: 
+            obj.delete()
+            raise PermissionDenied(detail='You are no the author')
+
+        obj.recipe.instructions = obj
+        obj.recipe.save()
 
 
 class InstructionDetails(RetrieveUpdateAPIView):
@@ -49,7 +35,5 @@ class InstructionDetails(RetrieveUpdateAPIView):
     def perform_update(self, serializer):
         obj = serializer.save()
 
-        input_recipe_id = self.request.data['recipe']
-        recipe = Recipe.objects.get(id__exact=input_recipe_id)
-        recipe.instructions = obj
-        recipe.save()
+        obj.recipe.instructions = obj
+        obj.recipe.save()
