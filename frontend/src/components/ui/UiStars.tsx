c05448940b97ba20df55f5e8d938c1ecbb24e22f
eliyamelamed1 <eliyamelamed1@email.com>
2022-01-06 18:24:16 +0200

    added gray/orange stars based on rating

--- frontend/src/components/ui/UiStars.tsx
+++ frontend/src/components/ui/UiStars.tsx
@@ -4,31 +4,34 @@ import StarIcon from '../../assets/icons/star.svg';
 import styles from '../../styles/ui/UiStars.module.scss';
 
 const UiStars: React.FC<{ starsCount: string }> = ({ starsCount }) => {
-    // const totalStars = 5;
-    // const grayStar = (
-    //     <i className={styles.grayStar}>
-    //         {StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}
-    //     </i>
-    // );
+    const grayStar = (
+        <i className={styles.grayStar}>
+            {StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}
+        </i>
+    );
 
-    // const orangeStar = <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>;
-    // console.log(Math.round(starsCount));
+    const orangeStar = <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>;
 
-    // const mixedStars = () => {
-    //     Math.round(starsCount)
-    // };
+    const mixedStars = () => {
+        const mixedStarsArray = [];
+        const numOfOrangeStars = Math.round(+starsCount);
+        const numOfGrayStars = 5 - numOfOrangeStars;
 
-    // if (!starsCount) starsCount = 0.0;
+        for (let index = 0; index < numOfOrangeStars; index++) {
+            mixedStarsArray.push(orangeStar);
+        }
+        for (let index = 0; index < numOfGrayStars; index++) {
+            mixedStarsArray.push(grayStar);
+        }
+
+        return mixedStarsArray;
+    };
+
+    if (!starsCount) starsCount = '0.0';
 
     return (
         <section className={styles.rating}>
-            <div className={styles.rating_icons}>
-                <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>
-                <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>
-                <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>
-                <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>
-                <i>{StarIcon.src && <Image src={StarIcon.src} alt='star icon' height={100} width={100} />}</i>
-            </div>
+            <div className={styles.rating_icons}>{mixedStars()}</div>
             <div className={styles.rating_score}>
                 <span>({parseFloat(starsCount).toFixed(1)})</span>
             </div>
